a874390349b0ca4116be196c8cba7f2e6b408d95
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-02-18 23:33:13 +0700

    implematnitng firebase

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDqyXzWPFKR2k4wiH9iVGsiukJMTZGfgHw",
+    authDomain: "e-clone-7332d.firebaseapp.com",
+    projectId: "e-clone-7332d",
+    storageBucket: "e-clone-7332d.appspot.com",
+    messagingSenderId: "721995349364",
+    appId: "1:721995349364:web:9cd1915f7302b7fe600021"
+  };
